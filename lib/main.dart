5c3f1c4db7372a676e43cd103ad838309f9a9591
Ashish Kumar Bhoi <ashish.bhoi12@gmail.com>
2022-11-09 22:20:06 +0530

    material 3 added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:chatapp/pages/home_page.dart';
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,12 +11,37 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Chat App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage(),
+    ColorScheme? lightColorScheme;
+    ColorScheme? darkColorScheme;
+    return DynamicColorBuilder(
+      builder: (ColorScheme? lightDynamicColor, ColorScheme? darkDynamicScheme) {
+        if (lightDynamicColor != null || darkDynamicScheme != null) {
+          lightColorScheme = lightDynamicColor;
+          darkColorScheme = darkDynamicScheme;
+        } else {
+          lightColorScheme = ColorScheme.fromSeed(
+            seedColor: const Color(0xFF7A5900),
+            brightness: Brightness.light,
+          );
+          darkColorScheme = ColorScheme.fromSeed(
+            seedColor: const Color(0xFF7A5900),
+            brightness: Brightness.dark,
+          );
+        }
+        return MaterialApp(
+          title: 'Chat App',
+          theme: ThemeData(
+            colorScheme: lightColorScheme,
+            useMaterial3: true,
+          ),
+          darkTheme: ThemeData(
+            colorScheme: darkColorScheme,
+            useMaterial3: true,
+          ),
+          debugShowCheckedModeBanner: false,
+          home: const HomePage(),
+        );
+      }
     );
   }
 }
